114dfb04720d75aea1fa83f4ad6e6ff56d9f13ff
donny.hurley@atu.ie <donny.hurley@atu.ie>
2024-09-22 22:00:13 +0100

    Initial commit

---
+++ Week2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+            Clothes a = new Clothes("blue");
+            Shirt b = new Shirt("red", true);
+            Console.WriteLine(a.WhatType());
+            Console.WriteLine(b.WhatType());
+            Console.WriteLine(b);
+            Console.WriteLine(a);
+
+        }
+    }
+}
---
+++ Week2/clothes.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp9;
+
+public class Clothes
+{
+    public string colour { get; }
+    
+    public Clothes(string colour) {
+        this.colour = colour;
+    }
+
+    public virtual string WhatType() {
+        return "I am clothes with colour " + colour;
+    }
+
+    public override string ToString() {
+        return colour + " clothes";
+    }
+}
+
+public class Shirt : Clothes{
+    private bool longsleeves; 
+    public Shirt(string colour, bool longsleeves) : base(colour) {
+        this.longsleeves = longsleeves;
+    }
+
+    public override string WhatType() {
+        string result = "I am a ";
+        if (longsleeves)
+            result += "long ";
+        else
+            result += "short ";
+        result += "sleeved " + colour + " shirt";
+        return result;
+    }
+
+    public override string ToString() {
+        return colour + " shirt";
+    }
+}
+
