87f3c79633341362104a228d7a0c987522457b2a
donny.hurley@atu.ie <donny.hurley@atu.ie>
2024-09-26 10:31:23 +0100

    Update RentalCar.cs

--- Week2/RentalCarSystem/RentalCar.cs
+++ Week2/RentalCarSystem/RentalCar.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RentalCarSystem
+﻿namespace RentalCarSystem
 {
     internal class RentalCar : Vehicle
     {
